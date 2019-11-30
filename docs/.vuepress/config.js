a970179e5048e8085963c3d0d75cec1f877d32fd
Curtis <curtis@drumeo.com>
2019-11-29 22:47:20 -0800

    Add Vuepress build config

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    title: 'MusoraJS',
+    description: 'MusoraJS is a collaborative initiative to create a cross-platform collection of javascript packages',
+    evergreen: true,
+    themeConfig: {
+        displayAllHeaders: true,
+        repo: 'railroadmedia/musora-js',
+        nav: [
+            {
+                text: 'Guide',
+                link: '/'
+            },
+            {
+                text: 'Services',
+                link: '/services/'
+            },
+            {
+                text: 'Models',
+                link: '/models/'
+            },
+        ],
+        sidebar: 'auto',
+        lastUpdated: 'Last Updated',
+    },
+};
