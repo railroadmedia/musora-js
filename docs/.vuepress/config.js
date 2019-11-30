e6226a032187d5b7a9662aa88130b69857481b54
Curtis <curtis@drumeo.com>
2019-11-29 22:59:39 -0800

    Add base config for deployment

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,6 @@
 module.exports = {
     title: 'MusoraJS',
+    base: '/musora-js/',
     description: 'MusoraJS is a collaborative initiative to create a cross-platform collection of javascript packages',
     evergreen: true,
     themeConfig: {
